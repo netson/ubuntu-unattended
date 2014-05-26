14b2679d0023d3a8852bd71c892d66afd9cbdcc5
netson <support@netson.nl>
2014-05-26 16:26:38 +0200

    version 1
    supports ubuntu 12.04.4 and 14.04

--- README.md
+++ README.md
@@ -1,4 +1,123 @@
-ubuntu-unattended
-=================
+# Unattended Ubuntu ISO Maker
 
-Script to create unattende Ubuntu ISO
+This simple script will create an unattended Ubuntu ISO from start to finish. It will ask you a few questions once, and embed your answers into a remastered ISO file for you to use over and over again.
+
+This script creates a 100% original Ubuntu installation; no additional software is added (aside from the VMWare OSP Tools, which are optional), not even an ```apt-get update``` is performed. You have all the freedom in the world to customize your Ubuntu installation whichever way you see fit. This script just takes the pain out of re-installing Ubuntu over and over again.
+
+Consider using tools like chef or puppet to perform any additional software installations/configurations. 
+
+Created by: **Rinck Sonnenberg (Netson)**
+
+## Compatibility
+
+The script supports the following Ubuntu editions out of the box:
+
+* Ubuntu 12.04.4 Server LTS amd64 - Precise Pangolin
+* Ubuntu 14.04 Server LTS amd64 - Trusty Tahr
+
+This script has been tested on and with these two versions as well, but I see no reason why it shouldn't work with other Ubuntu editions. Other editions would require minor changes to the script though.
+
+## Usage
+
+* From your command line, run the following command:
+
+```
+$ sudo curl -sS https://raw.githubusercontent.com/netson/ubuntu-unattended/master/create-unattended-iso.sh | sh
+```
+
+* Choose which version you would like to remaster:
+
+```
+ +---------------------------------------------------+
+ |            UNATTENDED UBUNTU ISO MAKER            |
+ +---------------------------------------------------+
+
+ which ubuntu edition would you like to remaster:
+
+  [1] Ubuntu 12.04.4 LTS Server amd64 - Precise Pangolin
+  [2] Ubuntu 14.04 LTS Server amd64 - Trusty Tahr
+
+ please enter your preference: [1|2]:
+```
+
+* Enter your desired hostname; the default is *ubuntu*:
+
+```
+ please enter your preferred hostname: ubuntu
+```
+
+* Enter your desired timezone; the default is *Europe/Amsterdam*:
+
+```
+ please enter your preferred timezone: Europe/Amsterdam
+```
+
+* Enter your desired username; the default is *netson*:
+
+```
+ please enter your preferred username: netson
+```
+
+* Enter the password for your user account; the default is *empty*
+
+```
+ please enter your preferred password:
+```
+
+* Confirm your password:
+
+```
+ confirm your preferred password:
+```
+
+* Indicate whether or not you wish to auto-install the VMWare OSP Tools for Ubuntu:
+
+```
+ do you wish to install the VMware OSP tools? [y/n]: y
+```
+
+* Sit back and relax, while the script does the rest! :)
+
+## What it does
+
+This script does a buch of stuff, here's the quick walk-through:
+
+* It asks you for your preferences regarding the unattended ISO
+* Downloads the appropriate Ubuntu original ISO straight from the Ubuntu servers; if a file with the exact name exists, it will use that instead (so it won't download it more than once if you are creating several unattended ISO's with different defaults)
+* Downloads the netson preseed file; this file contains all the magic answers to auto-install ubuntu. It uses the following defaults for you (only showing most important, for details, simply check the seed file in this repository):
+ * Language/locale: en_US
+ * Keyboard layout: US International
+ * Root login disabled (so make sure you write down your default usernames' password!)
+ * Partitioning: LVM, full disk, single partition
+* Install the mkpasswd program (part of the whois package) to generate a hashed version of your password
+* Install the genisoimage program to generate the new ISO file
+* Mount the downloaded ISO image to a temporary folder
+* Copy the contents of the original ISO to a working directory
+* Set the default installer language
+* Add/update the preseed file
+* Add the autoinstall option to the installation menu
+* Generate the new ISO file
+* Cleanup
+* Show a summary of what happended:
+
+```  
+ installing required packages
+ remastering your iso file
+ creating the remastered iso
+ -----
+ finished remastering your ubuntu iso file
+ the new file is located at: /tmp/ubuntu-14.04-server-amd64-unattended.iso
+ your username is: netson
+ your password is: 
+ your hostname is: ubuntu
+ your timezone is: Europe/Amsterdam
+```
+
+### That's it, enjoy! :)
+
+## Troubleshooting
+
+If you run into any issues, please create an issue; I am by no means a shell/bash expert (far from it), and it took me a while to compile this script into something that's easy to use and just works, but I'm happy to help where I can! :)
+
+## License
+MIT
---
+++ create-unattended-iso.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+
+# file names & paths
+tmp="/tmp"  # destination folder to store the final iso file
+
+# define spinner function for slow tasks
+# courtesy of http://fitnr.com/showing-a-bash-spinner.html
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# define download function
+# courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
+download()
+{
+    local url=$1
+    echo -n "    "
+    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo -ne "\b\b\b\b"
+    echo " DONE"
+}
+
+# define function to check if program is installed
+# courtesy of https://gist.github.com/JamieMason/4761049
+function program_is_installed {
+    # set to 1 initially
+    local return_=1
+    # set to 0 if not found
+    type $1 >/dev/null 2>&1 || { local return_=0; }
+    # return value
+    echo $return_
+}
+
+# print a pretty header
+echo 
+echo " +---------------------------------------------------+"
+echo " |               UNATTENDED UBUNTU ISO               |"
+echo " +---------------------------------------------------+"
+echo 
+
+# ask whether to include vmware tools or not
+while true; do
+    echo " which ubuntu edition would you like to remaster:"
+    echo
+    echo "  [1] Ubuntu 12.04.4 LTS Server amd64 - Precise Pangolin"
+    echo "  [2] Ubuntu 14.04 LTS Server amd64 - Trusty Tahr"
+    echo
+    read -p " please enter your preference: [1|2]: " ubver
+    case $ubver in
+        [1]* )  download_file="ubuntu-12.04.4-server-amd64.iso"           # filename of the iso to be downloaded
+                download_location="http://releases.ubuntu.com/12.04/"     # location of the file to be downloaded
+                new_iso_name="ubuntu-12.04.4-server-amd64-unattended.iso" # filename of the new iso file to be created
+                break;;
+        [2]* )  download_file="ubuntu-14.04-server-amd64.iso"             # filename of the iso to be downloaded
+                download_location="http://releases.ubuntu.com/14.04/"     # location of the file to be downloaded
+                new_iso_name="ubuntu-14.04-server-amd64-unattended.iso"   # filename of the new iso file to be created
+                break;;
+        * ) echo " please answer [1] or [2]";;
+    esac
+done
+
+# ask the user questions about his/her preferences
+read -ep " please enter your preferred hostname: " -i "ubuntu" hostname
+read -ep " please enter your preferred timezone: " -i "Europe/Amsterdam" timezone
+read -ep " please enter your preferred username: " -i "netson" username
+read -sp " please enter your preferred password: " password
+printf "\n"
+read -sp " confirm your preferred password: " password2
+printf "\n"
+
+# ask whether to include vmware tools or not
+while true; do
+    read -p " do you wish to install the VMware OSP tools? [y/n]: " yn
+    case $yn in
+        [Yy]* ) include_vmware=1
+                break;;
+        [Nn]* ) include_vmware=0
+                break;;
+        * ) echo " please answer [y]es or [n]o.";;
+    esac
+done
+
+
+# check if the passwords match to prevent headaches
+if [[ "$password" != "$password2" ]]; then
+    echo " your passwords do not match; please restart the script and try again"
+    echo
+    exit
+fi
+
+# download the ubunto iso
+cd $tmp
+if [[ ! -f $tmp/$download_file ]]; then
+    echo -n " downloading $download_file: "
+    download "$download_location$download_file"
+fi
+
+# download netson seed file
+seed_file="netson.seed"
+if [[ ! -f $tmp/$seed_file ]]; then
+    echo -h " downloading $seed_file: "
+    download "https://github.com/netson/ubuntu-unattended/raw/master/$seed_file"
+fi
+
+# install required packages
+echo " installing required packages"
+if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mkisofs") -eq 0 ]; then
+    (apt-get update > /dev/null 2>&1) &
+    spinner $!
+    (apt-get -y install whois genisoimage> /dev/null 2>&1) &
+    spinner $!
+fi
+
+# create working folders
+echo " remastering your iso file"
+mkdir -p $tmp
+mkdir -p $tmp/iso_org
+mkdir -p $tmp/iso_new
+
+# mount the image
+if grep -qs $tmp/iso_org /proc/mounts ; then
+    echo " image is already mounted, continue"
+else
+    (mount -o loop $tmp/$download_file $tmp/iso_org > /dev/null 2>&1)
+fi
+
+# copy the iso contents to the working directory
+(cp -rT $tmp/iso_org $tmp/iso_new > /dev/null 2>&1) &
+spinner $!
+
+# set the language for the installation menu
+cd $tmp/iso_new
+echo en > $tmp/iso_new/isolinux/lang
+
+# copy the netson seed file to the iso
+cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
+
+# include vmware tools
+if [[ include_vmware -eq 1 ]]; then
+    echo '
+# install vmware tools
+d-i apt-setup/local0/repository                             string      http://packages.vmware.com/tools/esx/5.5latest/ubuntu precise main
+d-i apt-setup/local0/comment                                string      VMware OSP Tools
+d-i preseed/late_command                                    string      in-target sh -c "wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub -O - | apt-key add - ; wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub -O - | apt-key add - ; apt-get update"; apt-install vmware-tools-esx-nox' >> $tmp/iso_new/preseed/$seed_file
+fi
+
+# generate the password hash
+pwhash=$(echo $password | mkpasswd -s -m sha-512)
+
+# update the seed file to reflect the users' choices
+# the normal separator for sed is /, but both the password and the timezone may contain it
+# so instead, I am using @
+sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file
+sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file
+sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file
+sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file
+
+# calculate checksum for seed file
+seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file)
+
+# add the autoinstall option to the menu
+sed -i "/label install/ilabel autoinstall\n\
+  menu label ^Autoinstall NETSON Ubuntu Server\n\
+  kernel /install/vmlinuz\n\
+  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/netson.seed preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
+
+echo " creating the remastered iso"
+cd $tmp/iso_new
+(mkisofs -D -r -V "NETSON_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
+spinner $!
+
+# cleanup
+umount $tmp/iso_org
+rm -rf $tmp/iso_new
+rm -rf $tmp/iso_org
+
+# print info to user  
+echo " -----"
+echo " finished remastering your ubuntu iso file"
+echo " the new file is located at: $tmp/$new_iso_name"
+echo " your username is: $username"
+echo " your password is: $password"
+echo " your hostname is: $hostname"
+echo " your timezone is: $timezone"
+echo
+
+# unset vars
+unset username
+unset password
+unset hostname
+unset timezone
+unset pwhash
+unset download_file
+unset download_location
+unset new_iso_name
+unset tmp
+unset seed_file
