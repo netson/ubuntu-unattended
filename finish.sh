69be1ebd204e8eac6b274b9d522205f0aa3149a3
netson <support@netson.nl>
2014-07-12 14:19:40 +0200

    small fix for the finish script

--- finish.sh
+++ finish.sh
@@ -4,11 +4,11 @@ set -e
 echo " finishing your puppet installation ... "
 
 # run puppet config
-puppet resource package puppet ensure=latest > /dev/null
+# puppet resource package puppet ensure=latest > /dev/null
 puppet resource service puppet ensure=running enable=true > /dev/null
 puppet agent --enable > /dev/null
 puppet agent
-puppet agent --test
+# puppet agent --test
 
 # remove myself to prevent any unintended changes at a later stage
 rm $0
